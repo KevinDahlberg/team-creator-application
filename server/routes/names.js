6a438a156a251b73d462e28ff657da2941b86bf0
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-29 16:55:41 -0500

    added get and post routes server side

--- server/routes/names.js
+++ server/routes/names.js
@@ -5,17 +5,53 @@ var mongoose = require("mongoose");
 var NamesSchema = mongoose.Schema({
   name : String,
 });
-var Names = mongoose.model("Names", NamesSchema);
 
-//GET employees
+var GroupsSchema = mongoose.Schema({
+  project_num: Number,
+  group_num: Number,
+  names: Array
+});
+
+var Names = mongoose.model("Names", NamesSchema);
+var Groups = mongoose.model("Groups", GroupsSchema);
+
+//GET names
 router.get("/makeNewGroup", function(req,res){
-  //Get all employees
+  //Get all names
   Names.find(function(err, allNames){
     if(err){
       console.log(err);
       res.sendStatus(500);
     }
     res.send(allNames);
+  });
+});
+
+//get groups
+router.get("/makeNewGroup", function(req, res){
+  Groups.find(function(err, allGroups){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.send(allGroups);
+  });
+});
+
+//post Groups
+router.post("/post", function(req,res){
+  //Instance of the Model to be saved to the database
+  var groups = new Groups();
+  groups.project_num = req.body.project_num;
+  groups.group_num = req.body.group_num;
+  groups.names = req.body.names;
+  groups.save(function(err, savedGroups){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+
+    res.send(savedGroups);
   });
 });
 
