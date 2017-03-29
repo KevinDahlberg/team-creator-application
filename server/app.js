7b9b926eac2cef9ce76d92e361cef4d6753b8955
Bri Dickman <brianna.dickman@gmail.com>
2017-03-29 16:09:51 -0500

    server live. initial commit

--- server/app.js
+++ server/app.js
@@ -2,13 +2,13 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var names = require('./routes/names');
+var names = require('./routes/names.js');
 var port = 4000;
+
 
 //uses
 app.use(express.static('server/public'));
 app.use(express.static('server/public', {index: 'views/index.html'}));
-app.use('/', index);
 app.use('/names', names);
 
 //listening
