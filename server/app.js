68d69bff5e486f36f2d627c5930f427639557a06
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-29 16:29:21 -0500

    set up DB on app.js

--- server/app.js
+++ server/app.js
@@ -10,6 +10,22 @@ app.use(express.static('server/public', {index: 'views/index.html'}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/names', names);
 
+
+//Database Variables
+var mongoose = require("mongoose");
+var mongoURI = "mongodb://localhost:27017/kreator";
+var MongoDB = mongoose.connect(mongoURI).connection;
+
+//If there is an error connecting to the database, let us know!
+MongoDB.on("error", function(err){
+  console.log("Mongo Connection Error :" + err);
+});
+
+//If we successfully hooked up to the database, let us know!
+MongoDB.once("open", function(){
+  console.log("Connected to Mongo!");
+});
+
 //listening
 app.listen(port, function(){
   console.log("Listening to: ", port);
