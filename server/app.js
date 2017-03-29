06a62ffd063caf6ac6f08ae54994248ce7297116
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-29 16:10:26 -0500

    initial commit

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyParser = require('body-parser');
-var names = require('./routes/names');
+var names = require('./routes/names.js');
 var port = 4000;
 
 //uses
