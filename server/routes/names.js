693d242a69ae556e0b29dbb034cc4005a0b127da
Kevin Dahlberg <nsu_tpt@hotmail.com>
2017-03-29 16:26:38 -0500

    mvp get function created

--- server/routes/names.js
+++ server/routes/names.js
@@ -2,8 +2,21 @@ var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
 
-var NameSchema = mongoose.Schema({
+var NamesSchema = mongoose.Schema({
   name : String,
+});
+var Names = mongoose.model("Names", NamesSchema);
+
+//GET employees
+router.get("/", function(req,res){
+  //Get all employees
+  Names.find(function(err, allNames){
+    if(err){
+      console.log(err);
+      res.sendStatus(500);
+    }
+    res.send(allNames);
+  });
 });
 
 module.exports = router;
